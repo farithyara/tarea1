692ac9441188ecda05546646beb3ca909a58e6b7
juane <juane@LAPTOP-87OUM3TE>
2025-04-28 18:20:04 -0500

    Se añadieron los comentarios en las lineas de código y se complemento lo que hacia falta

--- src/javaapplication10/JavaApplication10.java
+++ src/javaapplication10/JavaApplication10.java
@@ -1,21 +1,41 @@
 package javaapplication10;
+
 import java.util.Scanner;
+import java.util.Base64; // Importamos librería para codificar en Base64
+
 public class JavaApplication10 {
     public static void main(String[] args) {
-        Scanner lectura = new Scanner(System.in);
-        System.out.println("Escriba su nombre: ");
-        String nombre = lectura.next();
-        System.out.println("Escriba su carrera: ");
-        String carrera = lectura.next();
-        System.out.println("Ingrese su edad: ");
-        String edad = lectura.next();
-        Integer.valueOf(edad);
-        Float.valueOf(edad);
-        Double.valueOf(edad);
-        System.out.println("Ingrese su telefono: ");
-        Integer telefono = lectura.nextInt();
-        telefono.toString();
+        Scanner lectura = new Scanner(System.in); // Crear objeto Scanner para entrada de datos
         
+        System.out.println("Escriba su nombre: "); // Se pide el nombre
+        String nombre = lectura.next(); // Se lee nombre como String
+        
+        // Convertimos nombre a Base64
+        String nombreBase64 = Base64.getEncoder().encodeToString(nombre.getBytes());
+        
+        System.out.println("Escriba su carrera: "); // Se pregunta la carrera
+        String carrera = lectura.next(); // Leer carrera como String
+        
+        System.out.println("Ingrese su edad: "); // Se pregunta edad
+        String edadTexto = lectura.next(); // Para leer edad como String
+        
+        // Convertimos edad a Integer y Float
+        Integer edadEntero = Integer.valueOf(edadTexto);
+        Float edadDecimal = Float.valueOf(edadTexto);
+        
+        System.out.println("Ingrese su telefono: "); // Se pide el teléfono
+        Integer telefono = lectura.nextInt(); // Para que lea teléfono como Integer
+        
+        // Convertimos teléfono a Double y luego a String
+        Double telefonoDouble = Double.valueOf(telefono);
+        String telefonoTexto = telefono.toString();
+        //Con Integer hay un límite en el rango que acepta la variable
+        // Para que se muestren los resultados
+        System.out.println("Nombre en Base64: " + nombreBase64);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("Edad como Integer: " + edadEntero);
+        System.out.println("Edad como Float: " + edadDecimal);
+        System.out.println("Teléfono como Double: " + telefonoDouble);
+        System.out.println("Teléfono como String: " + telefonoTexto);
     }
-    
 }
